91225c7c4cf2ee5946e0b2617fd7de4cbe9096a7
rand0m0User <79854039+rand0m0User@users.noreply.github.com>
2023-10-05 10:25:12 -0700

    update post processor script: extended cleanup

--- repack build jar.bat
+++ repack build jar.bat
@@ -1,5 +1,16 @@
 @echo off
 color 0a
-cd "build\libs"
+cd "build"
+
+"C:\Program Files\7-Zip\7z" a -sdel tmp.null "classes"
+del tmp.null
+"C:\Program Files\7-Zip\7z" a -sdel tmp.null "generated"
+del tmp.null
+"C:\Program Files\7-Zip\7z" a -sdel tmp.null "resources"
+del tmp.null
+"C:\Program Files\7-Zip\7z" a -sdel tmp.null "tmp"
+del tmp.null
+
+cd "libs"
 del *-dev.jar
 "..\..\advzip.exe" -z -3 *.jar
